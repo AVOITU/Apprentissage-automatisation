f1991d21b7ac0e345fa915bff564095432dcdbc5
Antoine Voituron <avoitu@orange.fr>
2023-09-03 18:43:46 +0200

    integration of the working function first_configuration, and appelaings of functions in main

--- automatisationgit.py
+++ automatisationgit.py
@@ -3,39 +3,53 @@ import os
 import tkinter as tk
 from tkinter import simpledialog
 
-def first_configuration():
-      #configure the path to git bash and stock it
-    if not os.path.exists("git_bash_path"):
+def working_directory():
+    #define the working directory because the file created in configuration is not writing with the python file
+    for root, dirs, files in os.walk('/'):
+        if "automatisationgit.py" in files:
+            os.chdir(root)
+
+
+def configuration_file():
+        #search the path to git bash, write it in "git_bash_path.txt" in order to stock it.
         try:
-            for root, dirs, files in os.walk("C:"):
+            for root, dirs, files in os.walk('/'):
                 if "git-bash.exe" in files:
                     git_bash_path=f"{root}\git-bash.exe"
-                    print(git_bash_path)
-                    with open("git_bash_path","w") as git_bash_file:
+                    with open("git_bash_path.txt","w") as git_bash_file:
                         git_bash_file.write(git_bash_path)
                     break
         except ValueError:                   
-            print("copy-past the git bash path:")
-            git_bash_path=input()
-            with open("git_bash_path","w") as git_bash_file:
-                git_bash_file.write(git_bash_path)
+            print("Git bash not found, verify the installation of the software")
 
 
-def local_path():
+def first_configuration():
+    #search for a previous file with git bash path, write it if absent, return the path for the continuation of the programm if present
+    if not os.path.exists("git_bash_path.txt"):
+        working_directory()
+        configuration_file()
+    else:
+        with open("git_bash_path.txt","r") as git_bash_file:
+            git_bash_path=git_bash_file.read()
+
+    return git_bash_path
+
+
+def local_path(git_bash_path):
     #ask the path from the user to separate it and prepare the command cd to bring git at the right place
     print("copy-past the path of your local repository")
     local_path=input()
     local_path=local_path.split('\\')
     for path in local_path:
-        subprocess.run(["C:\Program Files\Git\git-bash.exe","-c","cd",path])
+        subprocess.run([git_bash_path,"-c","cd",path])
 
 def init():
     subprocess.run()
 
 
 def main():
-    first_configuration()
-    local_path()
+    git_bash_path=first_configuration()
+    local_path(git_bash_path)
 
 
 main()
