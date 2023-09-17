5387a11d92fb7a00d17f4881890beb3e5153b658
Antoine Voituron <avoitu@orange.fr>
2023-09-17 17:11:56 +0200

    simple commands with front end

--- complexcommands.py
+++ complexcommands.py
@@ -1,5 +1,65 @@
+import first_configuration
+import simplecommands
+from tkinter import simpledialog
+import tkinter as tk
+
+def user_demand():
+    fenetre_principale = tk.Tk()
+    fenetre_principale.title("Fonctions")
+    fenetre_principale.geometry("800x600")
+
+    button_repository_initialisation = tk.Button(fenetre_principale, text="Repository initialisation", command=lambda: repository_initialisation(git_bash_path, file_path, message_commit))
+    button_branch_creation = tk.Button(fenetre_principale, text="Branch creation", command=lambda: branch_creation(git_bash_path, branch_checkout))
+    button_update_file = tk.Button(fenetre_principale, text="Update file", command=lambda: update_file(git_bash_path, message_commit))
+
+    button_repository_initialisation.grid(row=0, column=0)
+    button_branch_creation.grid(row=1, column=0)
+    button_update_file.grid(row=2, column=0)
+
+    fenetre_principale.mainloop()
+
+def repository_initialisation(git_bash_path, file_path, message_commit):
+    file_name=file_path.split('\\')
+    simplecommands.init(git_bash_path)
+    simplecommands.link_repo(git_bash_path)
+    message_commit=f'first commit to create {file_name}.py, link repositories and initiate GitHub repository'
+    simplecommands.first_touch_add_commit_push(git_bash_path, file_name, message_commit)
+    
+def branch_creation(git_bash_path, branch_checkout):
+    branch_checkout=simpledialog.askstring("branch_creation", "Enter the name of your parent branch:")
+    simplecommands.checkout(git_bash_path, branch_checkout)
+    branch_checkout=simplecommands.new_branch(git_bash_path)
+    simplecommands.checkout(git_bash_path, branch_checkout)
+    message_commit=f"creation of an empty file to write the code for '{branch_checkout}'"
+    file_name=[branch_checkout]
+    simplecommands.first_touch_add_commit_push(git_bash_path, file_name, message_commit)
+
+def update_file(git_bash_path, message_commit):
+    file_name=simpledialog.askstring("Local path", "Enter file to update:")
+    branch_checkout=file_name
+    simplecommands.checkout(git_bash_path, branch_checkout)
+    simplecommands.add(git_bash_path, file_name)
+    simplecommands.commit(git_bash_path, message_commit)
+    simplecommands.push(git_bash_path, file_name)
 
 
-def complexcommands():
+# def initially used to back end functionnment, no longer necessary
+def complexcommands(git_bash_path, branch_checkout, message_commit, user_choice):
+    try:
+        match user_choice:
+            case "repository_initialisation":
+                repository_initialisation(git_bash_path, file_path, message_commit)
+            case "branch_creation":
+                branch_creation(git_bash_path, branch_checkout)
+            case "update_file":
+                update_file(git_bash_path, message_commit)
+            case _:
+                file_path=simpledialog.askstring("Erreur", "Invalid input")    
+    except Exception as e:
+        simpledialog.askstring("Local path", f"{e}")
 
-complexcommands()
+if __name__ == "__main__":
+    message_commit=0
+    branch_checkout=0
+    root, git_bash_path, file_path=first_configuration.first_configuration()
+    user_demand()
--- simplecommands.py
+++ simplecommands.py
@@ -1,6 +1,6 @@
-import os
 import subprocess
 from first_configuration import first_configuration
+from tkinter import simpledialog
 
 #define empty variable for some functions in order to ask name for the user
 message_commit=0
@@ -10,19 +10,19 @@ def init(git_bash_path):
     #local repository initialisation and rename the master branch in main branch
     subprocess.run([git_bash_path,"-c","git init"])
     subprocess.run([git_bash_path,"-c","git branch -M main"])
+        
 
 def touch(git_bash_path,file_name):
     #create file based on name given by the user, name=["ok"] for test but is usually defined by the branch or the folder
     file_name=file_name[-1]
     subprocess.run([git_bash_path,"-c",f"touch '{file_name}.py'"])
     with open(f"{file_name}.py","w") as touch_file:
-        touch_file.write(f"\n\ndef {file_name}():\n\n{file_name}()")
+        touch_file.write(f"\ndef():\ndef {file_name}():\n{file_name}()")
     return file_name 
 
 def link_repo(git_bash_path):
     #ask user for the path to the GitHub repository
-    print("Enter an URL for your GitHub repository:")
-    URL_repo_github=input()
+    URL_repo_github=simpledialog.askstring("Link repo", "Enter an URL for your GitHub repository:")
     subprocess.run([git_bash_path,"-c",f"git remote add origin '{URL_repo_github}'"])
     return URL_repo_github
 
@@ -33,11 +33,10 @@ def add(git_bash_path, file_name):
 def commit(git_bash_path, message_commit):
     #if there is no message commit define by another function before ask it to the user and commit
     if not message_commit==0:
-        subprocess.run([git_bash_path, "-c", f'git commit -m "{message_commit}"'])
+        subprocess.run([git_bash_path, "-c", f'git commit -m "{message_commit}">sortie.txt'])
     else :
-        print("Enter your commit message:")
-        message_commit=input()
-        subprocess.run([git_bash_path, "-c", f'git commit -m "{message_commit}"'])
+        message_commit=simpledialog.askstring("Commit", "Enter your commit message:")
+        subprocess.run([git_bash_path, "-c", f'git commit -m "{message_commit}">sortie.txt'])
 
 def push(git_bash_path, file_name):
     #to push the files
@@ -45,16 +44,14 @@ def push(git_bash_path, file_name):
 
 def new_branch(git_bash_path):
     #create new branch
-    print("Enter your new branch name:")
-    branch_name=input()
+    branch_name=simpledialog.askstring("New branch", "Enter your new branch name:")
     subprocess.run([git_bash_path,"-c",f"git branch '{branch_name}'"])
     return branch_name
 
 def checkout(git_bash_path, branch_checkout):
     #if checkout is precised is another function checkout to this branch otherwise ask the user for the name
     if branch_checkout==0:
-        print("Enter your destination branch:")
-        branch_checkout=input()
+        branch_checkout=simpledialog.askstring("Checkout", "Enter your destination branch:")
         subprocess.run([git_bash_path,"-c",f"git checkout '{branch_checkout}'"])
     else:
         subprocess.run([git_bash_path,"-c",f"git checkout '{branch_checkout}'"])
